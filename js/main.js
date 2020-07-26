a9c446a11ca26ddd50b4919fb7f205032f4aaabc
kwarchal <kamilwarchal295@gmail.com>
2020-07-26 17:22:05 +0200

    poprawa2

--- js/main.js
+++ js/main.js
@@ -6,12 +6,11 @@ function main(liczba, liczba2){
     let wynik = liczba + liczba2;
 
     if(liczba == 20 || liczba2 == 20 || wynik <= 20) {
-
-      }
-
-
-    return (liczba == 20 || liczba2 == 20 || wynik <= 20);
-
+          return true;
+        } else {
+          return false;
+        }
     }
+  
 
 console.log(main(20, 1))
